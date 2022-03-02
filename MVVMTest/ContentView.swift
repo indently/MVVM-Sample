804a3b403c90903e1cf29df8b5a15265299334f9
Federico <codepalace@fastmail.se>
2022-03-02 09:09:19 +0100

    Initial Commit

---
+++ MVVMTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVMTest
+//
+//  Created by Federico on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVMTest/MVVMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTestApp.swift
+//  MVVMTest
+//
+//  Created by Federico on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
